f6f66d6f8c987c85d856e6bf04240044c7455dbf
bhatt098 <55306347+bhatt098@users.noreply.github.com>
2020-08-13 19:44:51 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "sql12.freemysqlhosting.net";
-$db_user = "sql12357946";
-$db_password = "WKmSdhPEWr";
-$db_name = "sql12357946";
+$db_host = "remotemysql.com";
+$db_user = "2rBufOveB6";
+$db_password = "LsC9k02Kiv";
+$db_name = "2rBufOveB6";
 
 // Create Connection
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
